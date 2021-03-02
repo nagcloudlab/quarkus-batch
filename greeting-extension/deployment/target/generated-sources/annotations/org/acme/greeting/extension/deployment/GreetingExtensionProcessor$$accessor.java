fdcb0d527ce716c3f24aaf86b13bfc210a0ac957
Nagabhushanam <nagabhushanamn@gmail.com>
2021-03-02 14:05:52 +0530

    clean

--- greeting-extension/deployment/target/generated-sources/annotations/org/acme/greeting/extension/deployment/GreetingExtensionProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package org.acme.greeting.extension.deployment;
-public final class GreetingExtensionProcessor$$accessor {
-    private GreetingExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new GreetingExtensionProcessor();
-    }
-}
