3cf7c8a781f560d74232cc66f2f87ac74c59fb72
Nagabhushanam <nagabhushanamn@gmail.com>
2021-03-04 12:46:54 +0530

    microservices with quarkus

---
+++ super-heroes/rest-hero/src/main/java/io/quarkus/workshop/superheroes/hero/HeroResource.java
@@ -0,0 +1,110 @@
+// tag::adocResource[]
+package io.quarkus.workshop.superheroes.hero;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+
+// tag::adocOpenAPIImports[]
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+// end::adocOpenAPIImports[]
+// tag::adocResource[]
+import org.jboss.logging.Logger;
+
+@Path("/api/heroes")
+@Produces(APPLICATION_JSON)
+public class HeroResource {
+
+    private static final Logger LOGGER = Logger.getLogger(HeroResource.class);
+
+    @Inject
+    HeroService service;
+
+    @GET
+    @Path("/random")
+    public Response getRandomHero() {
+        Hero hero = service.findRandomHero();
+        LOGGER.debug("Found random hero " + hero);
+        return Response.ok(hero).build();
+    }
+
+    @GET
+    public Response getAllHeroes() {
+        List<Hero> heroes = service.findAllHeroes();
+        LOGGER.debug("Total number of heroes " + heroes);
+        return Response.ok(heroes).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getHero(
+        // tag::adocOpenAPI[]
+        @Parameter(description = "Hero identifier", required = true)
+        // end::adocOpenAPI[]
+        @PathParam("id") Long id) {
+        Hero hero = service.findHeroById(id);
+        if (hero != null) {
+            LOGGER.debug("Found hero " + hero);
+            return Response.ok(hero).build();
+        } else {
+            LOGGER.debug("No hero found with id " + id);
+            return Response.noContent().build();
+        }
+    }
+
+    @POST
+    public Response createHero(
+        @Valid Hero hero, @Context UriInfo uriInfo) {
+        hero = service.persistHero(hero);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(Long.toString(hero.id));
+        LOGGER.debug("New hero created with URI " + builder.build().toString());
+        return Response.created(builder.build()).build();
+    }
+
+    @PUT
+    public Response updateHero(
+        @Valid Hero hero) {
+        hero = service.updateHero(hero);
+        LOGGER.debug("Hero updated with new valued " + hero);
+        return Response.ok(hero).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteHero(
+        @PathParam("id") Long id) {
+        service.deleteHero(id);
+        LOGGER.debug("Hero deleted with " + id);
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Produces(TEXT_PLAIN)
+    @Path("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
